cc41d78c444501a24e28157db04c8ad1dcc22821
vbenavente <vbenavente@hotmail.com>
2016-03-16 10:35:08 -0700

    first if else if else added

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ alert('Hi ' + userName + ', Welcome to my page!');
 
 var playGame = prompt('Do you want to play a game? yes or no');
 console.log('You actually do not have a choice! ' + playGame);
+if((playGame.toLowerCase() === 'yes') || (playGame.toLowerCase() === 'y')) {
+  alert('Great ' + userName + ' lets play!');
+} else if((playGame.toLowerCase() === 'no') || (playGame.toLowerCase() === 'n')) {
+  alert('Ok ' + userName + ' I will just ask you a few questions?');
+} else {console.log('You cannot even answer a simple question?');
+  alert('I did not understand your response');
+}
 
 var manyLanguages = prompt('Do you think I have studied more than 5 languages? yes or no');
 console.log('I have actually studied 9 languages ' + manyLanguages);
