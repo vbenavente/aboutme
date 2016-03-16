f79cfb11c80039154af17baa83ca7cfb992488e1
vbenavente <vbenavente@hotmail.com>
2016-03-16 10:58:52 -0700

    5 questions complete code working

--- app.js
+++ app.js
@@ -17,12 +17,40 @@ if((playGame.toLowerCase() === 'yes') || (playGame.toLowerCase() === 'y')) {
 
 var manyLanguages = prompt('Do you think I have studied more than 5 languages? yes or no');
 console.log('I have actually studied 9 languages ' + manyLanguages);
+if((manyLanguages.toLowerCase() === 'yes') || (manyLanguages.toLowerCase() === 'y')) {
+  alert('That is correct ' + userName);
+} else if ((manyLanguages.toLowerCase() === 'no') || (manyLanguages.toLowerCase() === 'n')) {
+  alert ('Nope you are wrong ' + userName);
+} else {console.log('You cannot even answer a simple question?');
+  alert('I did not understand your response');
+}
 
 var manyCountries = prompt('Do you think I have been to more than 5 countries? yes or no');
 console.log('I have been to more than 5 countries ' + manyCountries);
-
-var birthCity = prompt('Do you think I was born in Asheville, NC? yes or no');
-console.log('I was actually born in Raleigh, NC ' + birthCity);
+if((manyCountries.toLowerCase() === 'yes') || (manyCountries.toLowerCase() === 'y')) {
+  alert('That is correct ' + userName);
+} else if ((manyCountries.toLowerCase() === 'no') || (manyCountries.toLowerCase() === 'n')) {
+  alert ('Nope you are wrong ' + userName);
+} else {console.log('You cannot even answer a simple question?');
+  alert('I did not understand your response');
+}
 
 var birthYear = prompt('Do you think I was born in the 70s? yes or no');
 console.log('I was born in 1981 ' + birthYear);
+if((birthYear.toLowerCase() === 'yes') || (birthYear.toLowerCase() === 'y')) {
+  alert('Nope you are wrong ' + userName);
+} else if ((birthYear.toLowerCase() === 'no') || (birthYear.toLowerCase() === 'n')) {
+  alert ('That is correct ' + userName);
+} else {console.log('You cannot even answer a simple question?');
+  alert('I did not understand your response');
+}
+
+var birthCity = prompt('Do you think I was born in Asheville, NC? yes or no');
+console.log('I was actually born in Raleigh, NC ' + birthCity);
+if((birthCity.toLowerCase() === 'yes') || (birthCity.toLowerCase() === 'y')) {
+  alert('That is correct ' + userName);
+} else if ((birthCity.toLowerCase() === 'no') || (birthCity.toLowerCase() === 'n')) {
+  alert ('Nope you are wrong ' + userName);
+} else {console.log('You cannot even answer a simple question?');
+  alert('I did not understand your response');
+}
