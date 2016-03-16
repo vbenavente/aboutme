3964d37f72cd254bc698dd03762ff0d15a6f68e1
vbenavente <vbenavente@hotmail.com>
2016-03-16 08:37:37 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+/*console.log('hello world from javascript');
+alert('hello user I am javascript!');
+
+var favoriteNumber = prompt('What is your favorite number?');
+
+alert('your favorite number is ' + favoriteNumber);
+
+var slugsRule = confirm('are wizards better than slugs?');
+
+alert('wizards are better than slugs: ' + slugsRule);*/
+console.log('Tell me about your car!');
+
+var carColor = prompt('What color is your car?');
+console.log('So you drive a ' + carColor + ' car');
+
+var carMake = prompt('What make car do you drive?');
+console.log('It is a ' + carModel);
+
+var carModel = prompt('What model is your car?');
+console.log('Your ' + carModel + ' is a ' + carMake);
+
+var modelYear = prompt('What year is your car?');
+console.log('Your car was born in ' + modelYear);
+
+var carFuel = prompt('Does your car run on diesel or unleaded gas?');
+console.log('Your car runs on ' + carFuel + '. Mine runs on love!');
+
+var carDoors = prompt('How many doors does your car have?');
+console.log(carDoors + ' doors is better than one!');
+
+alert ('You drive a ' + carColor + ' ' + modelYear + ' ' + carMake + ' ' + carModel + ' that runs on ' + carFuel + ' and has ' + carDoors + ' doors.');
